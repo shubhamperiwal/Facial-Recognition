0beebb2fab60c14ee97b51c738412ac1d22580a0
Shubham Periwal <shubhamp.2016@smu.edu.sg>
2018-03-25 13:10:22 +0800

    Adding Kivy

--- Detector.py
+++ Detector.py
@@ -2,36 +2,44 @@ import cv2
 import numpy as np
 import sql_connector
 
-faceDetect = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
-cam=cv2.VideoCapture(0)
-#create recognizer
-rec = cv2.face.LBPHFaceRecognizer_create()
-#load training data so we have a trained recognizer
-rec.read("recognizer/trainingData.yml")
-ID=0
+def authorise():
+    #Boolean to decide wether authorised face or not
+    isAuthorised = False
 
-#create font and color of text
-fontface = cv2.FONT_HERSHEY_SIMPLEX
-fontscale = 1
-fontcolor = (255, 255, 255)
+    faceDetect = cv2.CascadeClassifier('cascades/haarcascade_frontalface_default.xml')
+    cam=cv2.VideoCapture(0)
+    #create recognizer
+    rec = cv2.face.LBPHFaceRecognizer_create()
+    #load training data so we have a trained recognizer
+    rec.read("recognizer/trainingData.yml")
+    ID=0
 
-while(True):
-    ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-    for(x,y,w,h) in faces:
-        cv2.rectangle (img, (x,y), (x+w, y+h), (0,0,255), 2)
-        ID, conf = rec.predict(gray[y:y+h, x:x+w])
-        #str(id) is the text you want to print next to the face. #(x, y+h) is location of text
-        name = sql_connector.retrieve(ID)
-        cv2.putText(img, name, (x,y+h), fontface, fontscale, fontcolor)         
-    cv2.imshow("Face", img)
-    if(cv2.waitKey(1)==ord('q')):
-        break
+    #create font and color of text
+    fontface = cv2.FONT_HERSHEY_SIMPLEX
+    fontscale = 1
+    fontcolor = (255, 255, 255)
 
-cam.release()
-cv2.destroyAllWindows()
+    while(True):
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+        for(x,y,w,h) in faces:
+            cv2.rectangle (img, (x,y), (x+w, y+h), (0,0,255), 2)
+            ID, conf = rec.predict(gray[y:y+h, x:x+w])
+            #str(id) is the text you want to print next to the face. #(x, y+h) is location of text
+            name = sql_connector.retrieve(ID)
+            cv2.putText(img, name, (x,y+h), fontface, fontscale, fontcolor)         
 
+            #Authorise if authorised person
+            if name == "Shubham":
+                isAuthorised = True
+                
+        cv2.imshow("Face", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+   
+    #Disconnect
+    cam.release()
+    cv2.destroyAllWindows()    
 
-
-
+    return isAuthorised
